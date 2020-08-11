1ba2db180887b4b24908fd06cdf037b84dda0a57
dcbon <dcbonitaa@gmail.com>
2020-08-11 16:38:07 +0700

    database all set

---
+++ server/models/task.js
@@ -0,0 +1,56 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Task extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Task.belongsTo(models.User)
+    }
+  };
+  Task.init({
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'Title cannot be empty'
+        },
+        notNull: {
+          msg: 'Title is required'
+        }
+      }
+    },
+    description: DataTypes.STRING(1234),
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'Category cannot be empty'
+        },
+        notNull: {
+          msg: 'Category is required'
+        }
+      }
+    },
+    UserId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    hooks: {
+      beforeCreate: (task, opt) => {
+        if (!task.description) task.description = 'No Description'
+      }
+    },
+    modelName: 'Task',
+  });
+  return Task;
+};
---
+++ server/models/user.js
@@ -0,0 +1,60 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      User.hasMany(models.Task)
+    }
+  };
+  User.init({
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'Invalid email format'
+        },
+        notEmpty: {
+          args: true,
+          msg: 'Email cannot be empty'
+        },
+        notNull: {
+          msg: 'Please enter your email'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [6, 12],
+          msg: 'Password must be 6 to 12 characters'
+        },
+        notNull: {
+          msg: 'Please enter your password'
+        }
+      }
+    },
+    organization: DataTypes.STRING
+  }, {
+    sequelize,
+    hooks: {
+      beforeCreate: (user, opt) => {
+        if (!user.organization) user.organization = 'Hacktiv8'
+      }
+    },
+    modelName: 'User',
+  });
+  return User;
+};
